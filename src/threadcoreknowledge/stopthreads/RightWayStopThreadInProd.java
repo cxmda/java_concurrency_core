fe8a7c462fed6e880ffed2781a99930d990b6a84
chenq <chenq@ktc.cn>
2020-05-28 14:08:40 +0800

    中断线程

--- src/threadcoreknowledge/stopthreads/RightWayStopThreadInProd.java
+++ src/threadcoreknowledge/stopthreads/RightWayStopThreadInProd.java
@@ -3,25 +3,20 @@ package threadcoreknowledge.stopthreads;
 /**
  * 最佳实践：catch了InterruptedExcetion之后的优先选择：在方法签名中抛出异常 那么在run()就会强制try/catch
  * @author chenqiang
- * @create 2020-05-28 13:59
+ * @create 2020-05-28 14:06
  */
 public class RightWayStopThreadInProd implements Runnable{
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(new RightWayStopThreadInProd());
-        thread.start();
-        Thread.sleep(1000);
-        thread.interrupt();
-    }
-
 
     @Override
     public void run() {
-        while (true && !Thread.currentThread().isInterrupted()){
+        while (true && !Thread.currentThread().isInterrupted()) {
             System.out.println("go");
             try {
                 throwInMethod();
             } catch (InterruptedException e) {
-                System.out.println("日志信息");
+                Thread.currentThread().interrupt();
+                //保存日志、停止程序
+                System.out.println("保存日志");
                 e.printStackTrace();
             }
         }
@@ -29,5 +24,12 @@ public class RightWayStopThreadInProd implements Runnable{
 
     private void throwInMethod() throws InterruptedException {
         Thread.sleep(2000);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new RightWayStopThreadInProd());
+        thread.start();
+        Thread.sleep(1000);
+        thread.interrupt();
     }
 }
--- src/threadcoreknowledge/stopthreads/RightWayStopThreadInProd2.java
+++ src/threadcoreknowledge/stopthreads/RightWayStopThreadInProd2.java
@@ -1,38 +1,38 @@
 package threadcoreknowledge.stopthreads;
 
 /**
- *  最佳实践：在catch语句中调用Thread.currentThread().interrupt()来恢复设置中断状态，
- *  以便于在后续的执行中，依然能够检查到刚才发生了中断回到刚才
- *  RightWayStopThreadInProd补上中断，让它跳出
+ * 描述：最佳实践2：在catch子语句中调用Thread.currentThread().interrupt()
+ * 来恢复设置中断状态，以便于在后续的执行中，依然能够检查到刚才发生
+ * 了中断回到刚才RightWayStopThreadInProd补上中断，让它跳出
  * @author chenqiang
- * @create 2020-05-28 14:00
+ * @create 2020-05-28 14:07
  */
 public class RightWayStopThreadInProd2 implements Runnable{
-
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(new RightWayStopThreadInProd2());
-        thread.start();
-        Thread.sleep(1000);
-        thread.interrupt();
-    }
 
     @Override
     public void run() {
         while (true) {
             if (Thread.currentThread().isInterrupted()) {
-                System.out.println("线程被中断了");
+                System.out.println("Interrupted，程序运行结束");
                 break;
             }
-            throwInMethod();
+            reInterrupt();
         }
     }
 
-    private void throwInMethod() {
+    private void reInterrupt() {
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new RightWayStopThreadInProd2());
+        thread.start();
+        Thread.sleep(1000);
+        thread.interrupt();
     }
 }
